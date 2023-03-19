4233c6d62c06ef6c89ce7932dc5d6548062f3d36
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-19 19:46:40 +0530

    New Admin Login Feature Was added

--- partials/_handlelogin.php
+++ partials/_handlelogin.php
@@ -1,3 +1,4 @@
+
 <?php
 $showError = "false";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -5,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['loginEmail'];
     $pass = $_POST['loginPass'];
 
-    $sql = "Select * from users where user_email='$email'";
+    $sql = "SELECT * FROM users WHERE user_email='$email'";
     $result = mysqli_query($conn, $sql);
     $numRows = mysqli_num_rows($result);
     if ($numRows == 1) {
@@ -15,8 +16,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['loggedin'] = true;
             $_SESSION['sno'] = $row['sno'];
             $_SESSION['useremail'] = $email;
+
+            // Check if the user is an admin
+            if ($email == 'admin@example.com') {
+                $_SESSION['isadmin'] = true;
+                header("Location: /Reflect-Forum/admin.php");
+            }
+        } else {
+            $showError = "Invalid Credentials";
         }
-        header("Location:  /forum/index.php");
+    } else {
+        $showError = "Invalid Credentials";
     }
-    header("Location: /forum/index.php");
+    if ($_SESSION['isadmin']) {
+        header("Location: /Reflect-Forum/admin.php");
+    } else {
+        header("Location: /Reflect-Forum/index.php?login=true&showError=$showError");
+    }
 }
+
