a9011be640f2e77cb3dae9ee950c2f952d7f109e
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-21 21:47:26 +0530

    Dashboard was created 70%

--- admin.php
+++ admin.php
@@ -8,9 +8,31 @@ include './partials/_dbconnect.php';
 // Fetch user details from the database
 $sql = "SELECT * FROM users";
 $result = mysqli_query($conn, $sql);
+
 ?>
+
 <!-- Display the user details in a table -->
-<table style="border-collapse: collapse; width: 100%; max-width: 800px; margin: 0 auto;">
+
+<style type="text/css">
+p{
+padding: 7px;
+text-align: justify; }
+.left{
+    margin-top: 20px;
+    margin-left: 20px;
+width: 45%;
+float: left; }
+.right{
+    margin-top: 20px;
+    margin-right: 20px;
+width: 45%;
+float: right; }
+</style>
+
+
+<!-- Left Class -->
+<div class="left"> 
+<table style="border-collapse: collapse; width: 100%; max-width: 800px; margin: 0 auto; border: 1px solid #1a2436;">
     <thead>
         <tr>
             <th style="padding: 12px 15px; text-align: left; background-color: #f2f2f2; border-bottom: 1px solid #ddd;">Name</th>
@@ -26,25 +48,59 @@ $result = mysqli_query($conn, $sql);
                 <td style="padding: 12px 15px; border-bottom: 1px solid #ddd;">
                     <form action="" method="POST">
                         <input type="hidden" name="user_id" value="<?php echo $row['sno']; ?>">
-                        <button type="submit" name="delete_user" style="background-color: #dc3545; color: #fff; border: none; padding: 8px 12px; border-radius: 4px; cursor: pointer;">Delete</button>
+                        <button type="submit" name="delete_user" style="background-color: #036bfc; color: #fff; border: none; padding: 8px 12px; border-radius: 4px; cursor: pointer;">Delete</button>
                     </form>
                 </td>
             </tr>
         <?php } ?>
     </tbody>
 </table>
+</div>
 
+<style>
+.grid-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 10px;
+}
+
+.grid-item { 
+  background-color: white;
+  border: 1px solid #1a2436;
+  border-radius: 20px;
+  padding: 10px;
+}
+
+</style>
+<!-- Right Class -->
+<div class="right"> 
+<div class="grid-container" >
+
+  <div class="grid-item">
+    <p style="color: #036bfc; font-size: 40px; text-align: center; margin-bottom:30px"> Users<br> <?php include './partials/_totalUser.php' ?></p>
+  </div>
+  
+  <div class="grid-item">
+  <p style="color: #036bfc; font-size: 40px; text-align: center;"> Threads<Br> <?php include './partials/_totalThread.php' ?></p>
+  </div>
+
+  <div class="grid-item">
+  <p style="color: #036bfc; font-size: 40px; text-align: center;"> Comments<br> <?php include './partials/_totalComments.php'?></p>
+  </div>
+
+  <div class="grid-item">
+  <p style="color: #036bfc; font-size: 40px; text-align: center;"> Teachers<br> <?php include './partials/_totalTeachers.php'?></p>
+  </div>
+
+</div>
+</div>
 
 <?php
-// Handle the delete user functionality
 if (isset($_POST['delete_user'])) {
     $user_id = $_POST['user_id'];
-
-    // Delete the user record from the database
     $sql = "DELETE FROM users WHERE sno='$user_id'";
     $result = mysqli_query($conn, $sql);
-
-    // Check if the query was successful
     if ($result) {
         echo "<script>alert('User Delete Successfully');</script>";
     } else {
