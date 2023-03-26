a9bf4a5572913bd5c36ebddcacfa34436c7ffdf6
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-26 21:52:35 +0530

    _handelLogin.php

--- partials/_handlelogin.php
+++ partials/_handlelogin.php
@@ -5,7 +5,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include '_dbconnect.php';
     $email = $_POST['loginEmail'];
     $pass = $_POST['loginPass'];
-
     $sql = "SELECT * FROM users WHERE user_email='$email'";
     $result = mysqli_query($conn, $sql);
     $numRows = mysqli_num_rows($result);
