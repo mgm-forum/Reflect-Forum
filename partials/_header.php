70c754c0ecc310e04e36f83652a901d3f15a1259
Harshal Tupe <harshaltupe12@gmail.com>
2023-04-26 23:06:15 +0530

    Alumni Regestration Added

--- partials/_header.php
+++ partials/_header.php
@@ -4,7 +4,7 @@ include 'partials/_dbconnect.php';
 include 'partials/_loginmodal.php';
 include 'partials/_signmodal.php';
 include 'partials/_teacherLogin.php';
-
+include 'partials/_alumniLogin.php';
 
 echo '<nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #161A1D">
     <div class="container-fluid">
@@ -12,11 +12,10 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark " style="background-color:
             <img src="./res/logo4.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
             Reflect Forum
         </a>
-
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="/Reflect-Forum/index.php">Home</a>
@@ -27,13 +26,12 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark " style="background-color:
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#exampleModal">Teacher Login</a></li>
-                        <li><a class="dropdown-item" href="https://www.gppen.ac.in/principal_desk">Alumni Login</a></li>
+                        <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#AlumniModal">Alumni Login</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Contact Us</a>
                 </li>
-
             </ul>
             ';
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
@@ -48,8 +46,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
                   <button class="btn btn-outline-warning me-2" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
                   <button class="btn btn-outline-warning me-2" data-bs-toggle="modal" data-bs-target="#signModal">Register</button>
                   <button class="btn btn-outline-warning me-2" data-bs-toggle="modal" data-bs-target="#loginModal">Admin</button>
-                  ';
-                  
+                  ';  
 }
 echo '</div>
     </div>
